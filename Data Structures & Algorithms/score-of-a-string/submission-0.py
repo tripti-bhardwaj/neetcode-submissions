5b827de38cc4123180b58a91b8b3c95091f23d37
Tripti Bhardwaj <63348938+tripti-bhardwaj@users.noreply.github.com>
2026-06-24 20:43:16 +0530

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        score = 0
+        s = list(s)
+        for ch in range(len(s) - 1):
+            score += abs(ord(s[ch + 1]) - ord(s[ch]))
+        return score
+
